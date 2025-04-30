1071924de34c6a0f05d7460bcc91d5015412d688
Andre Vinicius <avbds2077@gmail.com>
2025-04-30 16:50:29 -0300

    feat: add shadcn setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPage = () => {
+  return (
+    <div className="p-5 border-red-500 rounded-xl">
+      <h1 className="text-red-500">Products za warudo</h1>
+      <Button>FSW 7.0</Button>
+      <Input />
+    </div>
+  );
+};
+
+export default ProductPage;
