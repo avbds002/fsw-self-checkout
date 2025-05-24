8d4aafba45a14d514c68dcdfe2e07ccc602d2f3d
avbds002 <avbds2077@gmail.com>
2025-05-24 17:29:09 -0300

    feat: add products and categories ui

--- src/data/get-restaurant-by-slug.ts
+++ src/data/get-restaurant-by-slug.ts
@@ -3,7 +3,10 @@ import { notFound } from "next/navigation";
 import { db } from "@/lib/prisma";
 
 export const getRestaurantBySlug = async (slug: string) => {
-  const restaurant = await db.restaurant.findFirst({ where: { slug } });
+  const restaurant = await db.restaurant.findFirst({
+    where: { slug },
+    include: { menuCategories: { include: { products: true } } },
+  });
 
   if (!restaurant) {
     return notFound();
